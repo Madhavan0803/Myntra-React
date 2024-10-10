9ee50863e6eb4d741b6e880235a0d9a4335f4734
Madhavan0803 <Madhavanraj123@gmail.com>
2024-10-10 23:41:52 +0530

    uploaded files

---
+++ src/components/Autoscroll.jsx
@@ -0,0 +1,164 @@
+import React from 'react'
+import shoes from "../asserts/shoes.jpg"
+import Slipers from '../asserts/Slipers.webp'
+import shoes2 from '../asserts/shoes 2.jpg'
+import tshirstMen from '../asserts/tshirst men.webp'
+import watchMen from '../asserts/watch men.webp'
+import womenDress1 from '../asserts/women dress1.webp'
+import womenDress2 from '../asserts/women dress 2.webp'
+import watch2 from '../asserts/watch 2.jpeg'
+import menDress1 from '../asserts/men dress 1.avif'
+import slipper1 from '../asserts/slipper 1.webp'
+import tonPant from '../asserts/ton pant.avif'
+import watch1 from '../asserts/wtach 1.jpg'
+import braslet from '../asserts/braslet.jpg'
+import bresletMen from '../asserts/braslet men.webp'
+import tshirtWomen from '../asserts/tshirst women.webp'
+import shoes1 from '../asserts/shoes 1.avif'
+import heels1 from '../asserts/heels 1.webp'
+import heels from '../asserts/heels.webp'
+import womenDress3 from '../asserts/women dress 3.jpg'
+
+
+function Autoscroll() {
+  return (
+    <div class="Autoscroll">
+    <div class="f2">
+      <div class="f3">
+        <img src={shoes} alt="" />
+        <div class="f-title">
+          <h2>Red Tape</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={Slipers} alt="" />
+        <div class="f-title">
+          <h2>PLcoser</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={tshirstMen} alt="" />
+        <div class="f-title">
+          <h2>Tite</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={shoes2} alt="" />
+        <div class="f-title">
+          <h2>Zara</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={watchMen} alt="" />
+        <div class="f-title">
+          <h2>Sports</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={womenDress1} alt="" />
+        <div class="f-title">
+          <h2>Party</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={womenDress2} alt="" />
+        <div class="f-title">
+          <h2>Party</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={watch2} alt="" />
+        <div class="f-title">
+          <h2>Luxrious</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={menDress1} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={slipper1} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={tonPant} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={watch1} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={braslet} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={bresletMen} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={tshirtWomen} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={shoes1} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={heels1} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={womenDress3} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+
+      <div class="f3">
+        <img src={heels} alt="" />
+        <div class="f-title">
+          <h2>Bucket</h2>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default Autoscroll
